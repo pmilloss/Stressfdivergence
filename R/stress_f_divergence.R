d79f76b18f5d07231bc6a68566c9c12fd775467b
Pietro Millossovich <weekendwarrior71@gmail.com>
2023-11-18 00:27:47 +0100

    3

--- R/stress_f_divergence.R
+++ R/stress_f_divergence.R
@@ -1,15 +1,15 @@
 require(nleqslv)
 
 stress_mean_div <- function(x, new_mean, div = c("Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "user"), g = NULL, f.der = NULL, g.der = NULL, p = rep(1 / length(x), length(x)), alpha = NULL, trace = FALSE,...){
-  
+
   # f.der: first derivative of the divergence function
   # g: inverse function of f.der
   # g.der: first derivative of g (optional)
-  
+
   if (div == "user" & (is.null(g) | is.null(f.der))) stop("For a user defined divergence, the arguments 'g' and 'f.der' must be provided")
-  
+
   if (div == "Alpha" & is.null(alpha)) stop("For the Alpha divergence, the numeric argument 'alpha' must be provided")
-  
+
   if (div == "Chi2") {
     g <- function(x)0.5 * x
     f.der <- function(x)2 * x
@@ -21,7 +21,7 @@ stress_mean_div <- function(x, new_mean, div = c("Chi2", "KL", "Hellinger", "Alp
     f.der0 <- -Inf
     g.der <- function(x)g(x)
   } else if (div == "Hellinger") {
-    g <- function(x)1/(1 - x) ^ 2
+    g <- function(x)1 / (1 - x) ^ 2
     f.der <- function(x)(1 - 1 / sqrt(x))
     f.der0 <- -Inf
     g.der <- function(x)2 / (1 - x) ^ 3
@@ -42,18 +42,18 @@ stress_mean_div <- function(x, new_mean, div = c("Chi2", "KL", "Hellinger", "Alp
   } else if (div == "user") {
     f.der0 <- f.der(0)
   } else stop("The argument 'div' must be one of 'Chi2', 'KL', 'Hellinger', 'Alpha', 'Triangular', 'Jeffrey' or 'user' ")
-  
+
   constraints <- function(L){
     RN <- g(pmax(f.der0, L[1] + L[2] * x))
     F1 <- sum(p * RN) - 1
     F2 <- sum(p * RN * x) - new_mean
     return(c(F1, F2))
   }
-  
+
   starting <- c(L1 = f.der(1), L2 = 0)
-  
+
   if (!is.null(g.der)){
-    
+
     J <- function(L){
       RN.der <- g.der(L[1] + L[2] * x)
       ind <- (L[1] + L[2] * x > f.der0)
@@ -63,12 +63,12 @@ stress_mean_div <- function(x, new_mean, div = c("Chi2", "KL", "Hellinger", "Alp
       mat <- matrix(c(J11, J12, J12, J22), nrow = 2, byrow = TRUE)
       return(mat)
     }
-    
+
     res <- nleqslv(x = starting, fn = constraints, jac = J, ...)
   } else {
     res <- nleqslv(x = starting, fn = constraints, ...)
   }
-  # print(c(res$x[1], res$x[2])) 
+  # print(c(res$x[1], res$x[2]))
   # print(f.der0)
   #print(pmax(f.der0, res$x[1] + res$x[2] * x))
   RN <- g(pmax(f.der0, res$x[1] + res$x[2] * x))
@@ -107,7 +107,3 @@ plot(x, res_alpha2)
 
 res_trian <- stress_mean_div(x = x, new_mean = t, div = "Triangular", trace = T)# fails
 res_trian <- stress_mean_div(x = x, new_mean = t, div = "Triangular", trace = T, xscalm = "auto", control = list(ftol = 10e-15, xtol = 10e-15, maxit = 1000))# this works better
-
-
-
-
