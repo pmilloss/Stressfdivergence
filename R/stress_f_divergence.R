9507e6dfa8726eb9a500e42de58891c13a834840
Pietro Millossovich <weekendwarrior71@gmail.com>
2023-11-20 07:44:23 +0100

    7

--- R/stress_f_divergence.R
+++ R/stress_f_divergence.R
@@ -9,23 +9,25 @@
 #' @param m Numeric, the stressed moments of \code{f(x)}. Must be in the
 #' range of \code{f(x)}.
 #'
-#' @param div Character. One of "Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey" or "user". When a user specified divergence is chosen, the additional parameters "inv.div" (inverse of the divergence function) and "d.div" (derivative of the divergence function) must be passed. For the "Alpha" divergence, the numeric parameter "alpha" must be provided (when alpha is 1 the KL divergence is used)
+#' @param div Character. One of "Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey" or "user". When a user specified divergence is chosen, the additional parameters "inv.div" (inverse of the divergence function) and "d.div" (derivative of the divergence function) must be passed. For the "Alpha" divergence, the numeric parameter "alpha" must be provided (when alpha is 1 the KL divergence is used). +++++++++++++++++++ADD EQUATIONS FOR THE DIVERGENCES+++++++++++++++++
 #'
 #' @param inv.div A function specifying the inverse of the divergence function, when a user divergence function is used.
 #'
 #' @param d.div A function specifying the derivative of the divergence function, when a user divergence function is used.
 #'
-#' @param d.inv An optional function specifying the derivative of the inverse of the divergence function, when a user divergence function is used.
+#' @param d.inv An optional function specifying the derivative of the inverse of the divergence function, when a user divergence function is chosen.
 #'
-#' @param p a set of optional weights specifying the baseline model.
+#' @param p Numeric. A set of optional nonnegative scenario weights specifying the baseline model.
 #'
 #' @param normalise Logical. If true, values of \code{f(x)} are linearly scaled to the unit interval.
 #'
 #' @param show Logical. If true, print the result of the call to \code{\link[nleqslv]{nleqslv}}.
 #'
-#' @param start A numeric vector with two elements, the starting values for the+++++++
+#' @param names ++++++++++++
 #'
-#' @param sumRN Logical. If true, the++++++++++
+#' @param start A numeric vector with two elements, the starting values for the coefficients lambda1 and lambda2. Defaults to d.div(1) and 0 respectively, guaranteeing that the initial set of scenario weights
+#'
+#' @param sumRN Logical. If true, the scenario weights are normalized so as to average to 1.
 #'
 #' @param ... Additional arguments to be passed to \code{\link[nleqslv]{nleqslv}}.
 #'
@@ -34,7 +36,7 @@
 #' @return A named list
 #' @export
 
-stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "user"), inv.div = NULL, d.div = NULL, d.inv = NULL, p = rep(1 / length(x), length(x)), alpha = NULL, normalise = TRUE, show = FALSE, start = NULL, sumRN = FALSE, ...){
+stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "user"), inv.div = NULL, d.div = NULL, d.inv = NULL, p = rep(1 / length(x), length(x)), alpha = NULL, normalise = TRUE, show = FALSE, names = NULL, start = NULL, sumRN = FALSE, ...){
 
   if (is.SWIM(x)) x_data <- SWIM::get_data(x) else x_data <- as.matrix(x)
   if (anyNA(x_data)) warning("x contains NA")
@@ -47,7 +49,7 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
   z <- f(x_data[, k])
   min.fz <- min(z)
   max.fz <- max(z)
-  if (m < min.fz || m > max.fz) stop("m must be in the range of f(x)")
+  if (m < min.fz | m > max.fz) stop("m must be in the range of f(x)")
 
   if (normalise == TRUE) {
     z <- (z - min.fz) / (max.fz - min.fz)
@@ -86,6 +88,7 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
     # d.inv <- function(x)ifelse(x < 1, (1 - x) ^ -1.5, Inf)
     d.inv <- function(x)ifelse(x < 1, (1 - x) ^ -1.5, Inf)
   } else if (div == "Jeffrey") {
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++
   } else if (div == "user") {
     d.div0 <- d.div(0)
   } else stop("The argument 'div' must be one of 'Chi2', 'KL', 'Hellinger', 'Alpha', 'Triangular', 'Jeffrey' or 'user' ")
@@ -93,7 +96,7 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
   constr <- function(L){
     RN <- inv.div(pmax(d.div0, L[1] + L[2] * x))
     C1 <- sum(p * RN) - 1
-    C2 <- sum(p * RN * x) - m
+    C2 <- sum(p * RN * z) - m
     return(c(C1, C2))
   }
 
@@ -116,10 +119,10 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
 
   if (sol$termcd != 1) warning(paste("nleqslv terminated with code ", sol$termcd))
 
-  constr_moment <- list("k" = k, "m" = m, "f" = f)
+  constr_moment <- list("k" = k, "m" = m, "f" = f, "div" = div)
   constr <- list(constr_moment)
 
-  # Name stresses
+  # Name stresses +++++++++++++++++++++++++
   if (is.null(names)) {
     temp <- paste("stress", 1)
   } else {
@@ -128,10 +131,9 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
   if (length(temp) != 1) stop("length of names are not the same as the number of models")
   names(constr) <- temp
 
-
   if (is.null(colnames(x_data))) colnames(x_data) <-  paste("X", 1:ncol(x_data), sep = "")
 
-  w <- inv.div(pmax(d.div0, sol$x[1] + sol$x[2] * z))
+  w <- p * inv.div(pmax(d.div0, sol$x[1] + sol$x[2] * z)) # p here+++++++++++
   if(sumRN) w <- w / mean(w)
   new_weights = list()
   new_weights[[temp]] <- w
@@ -139,15 +141,17 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
   type <- list("moment") #++++++++++++++++++++
   my_list <- SWIM("x" = x_data, "new_weights" = new_weights, "type" = type, "specs" = constr)
   if (is.SWIM(x)) my_list <- merge(x, my_list)
+
   if (show == TRUE) print(sol)
-  m.ac <- colMeans(z * as.vector(exp(z %*% sol$x)))[-1]
+
+  m.ac <- sum(w * z)
   if (normalise == TRUE){
     m <- min.fz + (max.fz - min.fz) * m
     m.ac <- min.fz + (max.fz - min.fz) * m.ac
   }
   err <- m - m.ac
   rel.err <- (err / m) * (m != 0)
-  outcome <- data.frame(cols = as.character(k), required_moment = m, achieved_moment = m.ac, abs_error = err, rel_error = rel.err)
+  outcome <- c(required_moment = m, achieved_moment = m.ac, abs_error = err, rel_error = rel.err)
   print(outcome)
 
   if (log) {
@@ -157,33 +161,3 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m, div = c("Chi2", "KL",
   return(my_list)
 
 }
-
-set.seed(0)
-n <- 10 ^ 5
-x <- rlnorm(n, meanlog = 1, sdlog = 1)
-mean(x)
-stress <- 1.2
-t <- stress * mean(x)
-
-res_KL <- stress_mean_div(x = x, new_mean = t, div = "KL", trace = T)
-plot(x, res_KL)
-
-res_Chi2 <- stress_mean_div(x = x, new_mean = t, div = "Chi2", trace = T)
-plot(x, res_Chi2)
-
-res_Hell <- stress_mean_div(x = x, new_mean = t, div = "Hellinger", trace = T)# fails after inspection
-res_Hell1 <- stress_mean_div(x = x, new_mean = t, div = "Hellinger", trace = T, xscalm = "auto", control = list(ftol = 10e-15, xtol = 10e-15, maxit = 1000)) # works better, changing the scaling
-plot(x, res_Hell1, xlim = c(0, 150))
-plot(x, res_Hell1, xlim = c(0, 150), ylim = c(0, 10))
-lines(x, res_Hell, xlim = c(0, 150), ylim = c(0, 10))# madness
-
-res_alpha <- stress_mean_div(x = x, new_mean = t, div = "Alpha", alpha = -2, trace = T)
-plot(x, res_alpha)
-
-res_alpha2 <- stress_mean_div(x = x, new_mean = t, div = "Alpha", alpha = 0.5, trace = T) #ok
-res_alpha3 <- stress_mean_div(x = x, new_mean = t, div = "Alpha", alpha = 1.5, trace = T) #fails
-plot(x, res_alpha)
-plot(x, res_alpha2)
-
-res_trian <- stress_mean_div(x = x, new_mean = t, div = "Triangular", trace = T)# fails
-res_trian <- stress_mean_div(x = x, new_mean = t, div = "Triangular", trace = T, xscalm = "auto", control = list(ftol = 10e-15, xtol = 10e-15, maxit = 1000))# this works better
