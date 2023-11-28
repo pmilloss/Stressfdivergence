4023c25be65273ea3ef05d62bce4dfe34795b31a
Pietro Millossovich <weekendwarrior71@gmail.com>
2023-11-28 11:13:18 +1100

    20

--- R/stress_f_divergence.R
+++ R/stress_f_divergence.R
@@ -15,7 +15,7 @@
 #'
 #' @param dvg Character. One of "Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "LeCam" or "user". When a user specified divergence is chosen, the additional list 'div' containing ++++++++parameters "inv" (inverse of the first derivative of the divergence function) and "d.div0" (derivative of the divergence function) must be passed. For the "Alpha" divergence, the numeric parameter "alpha" must be provided (when alpha is 1 or 2 the KL, respectively the Chi2, divergence is used). +++++++++++++++++++ADD EQUATIONS FOR THE DIVERGENCES+++++++++++++++++
 #'
-#' @param div When a user divergence function is used, a named list containing at least the following objects: a function 'd.inv' giving the inverse of the derivative of the divergence function; a numeric 'd.div0' giving the the derivative at 0 of the divergence function (possibly -Inf); when 'theta' is specified (divergence constraint), a function 'div' giving the divergence function. Optionally, a function 'd.div' specifying the derivative of the divergence function.
+#' @param div.usr When a user divergence function is used, a named list containing at least the following objects: a function 'inv' giving the inverse of the first derivative of the divergence; a numeric 'div0' giving the the derivative at 0 of the divergence (possibly -Inf); when 'theta' is specified (divergence constraint), a function 'div' giving the divergence. Optionally, a function 'd.div' specifying the first derivative of the divergence.
 #'
 #' @param p Numeric. A set of optional nonnegative scenario weights specifying the baseline model.
 #'
@@ -42,7 +42,7 @@
 #' @return A named list
 #' @export
 
-stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, dvg = c("Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "LeCam", "user"), div = NULL, inv = NULL, d.div = NULL, d.inv = NULL, p = rep(1 / length(x), length(x)), alpha = NULL, normalise = TRUE, show = FALSE, names = NULL, start = NULL, sumRN = FALSE, log = FALSE, ...){
+stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, dvg = c("Chi2", "KL", "Hellinger", "Alpha", "Triangular", "Jeffrey", "LeCam", "user"), div.usr = NULL, p = rep(1 / length(x), length(x)), alpha = NULL, normalise = TRUE, show = FALSE, names = NULL, start = NULL, sumRN = FALSE, log = FALSE, ...){
 
   min.d <- !is.null(m)
   max.l <- !is.null(theta)
@@ -57,9 +57,9 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
 
   if (!is.numeric(p) | any(p < 0) | anyNA(p)) stop("p must be a vector of nonnegative weights") else p <- p / sum(p)
 
-  if (dvg == "user" & (!is.function(div$d.inv) | !is.numeric(div$d.div0))) stop("For a user defined divergence, the list 'div' must contain at least the objects 'd.inv' and 'd.div0' must be provided")
+  if (dvg == "user" & (!is.function(div.usr$inv) | !is.numeric(div.usr$div0))) stop("For a user defined divergence, the list 'div.usr' must contain at least the objects 'inv' and 'div0' must be provided")
 
-  if (dvg == "user" & max.l & !is.function(div$div)) stop("For a user defined divergence, when 'theta' is specified, the list 'div' must also contain the object 'div'")
+  if (dvg == "user" & max.l & !is.function(div.usr$div)) stop("For a user defined divergence, when 'theta' is specified, the list 'div.usr' must also contain the object 'div'")
 
   if (dvg == "Alpha" & (is.null(alpha) | !is.numeric(alpha))) stop("For the Alpha divergence, the numeric argument 'alpha' must be provided")
 
@@ -67,25 +67,25 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
     div <- function(x)x ^ 2 - 1
     d.div <- function(x)2 * x
     inv <- function(x)0.5 * x
-    d.div0 <- d.div(0)
+    div0 <- d.div(0)
     d.inv <- function(x)0.5
   } else if (dvg == "KL" | (dvg == "Alpha" & identical(alpha, 1))) {
     div <- function(x)ifelse(x > 0, x * log(x), 0)
     inv <- function(x)exp(x - 1)
     d.div <- function(x)1 + log(x)
-    d.div0 <- -Inf
-    d.inv <- function(x)inv.div(x)
+    div0 <- -Inf
+    d.inv <- function(x)inv(x)
   } else if (dvg == "Hellinger") {
     div <- function(x)(sqrt(x) - 1) ^ 2
     inv <- function(x)1 / (1 - x) ^ 2
     d.div <- function(x)1 - 1 / sqrt(x)
-    d.div0 <- -Inf
+    div0 <- -Inf
     d.inv <- function(x)2 / (1 - x) ^ 3
   } else if (dvg == "Alpha") {
     div <- function(x)(x ^ alpha - alpha * (x - 1) - 1) / (alpha * (alpha - 1))
     inv <- function(x)(-2 * x * (1 + alpha)) ^ (-2 / (1 + alpha))
     d.div <- function(x)(-0.5 * (x ^ (-0.5 * (1 + alpha))) / (1 + alpha))
-    d.div0 <- d.div(0)
+    div0 <- d.div(0)
     d.inv <- function(x)4 * (-2 * x * (1 + alpha)) ^ (- (3 + alpha) / (1 + alpha))
     dvg <- paste(dvg, " a=", alpha)
   } else if (dvg == "Triangular") {
@@ -93,7 +93,7 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
     inv <- function(x)-1 + 2 / sqrt(ifelse(x < 1, 1 - x, 0))
       # ifelse(x < 1, -1 + 2 / sqrt(1 - x), Inf)
     d.div <- function(x)(x - 1) * (x + 3) / (x + 1) ^ 2
-    d.div0 <- -3
+    div0 <- -3
     # d.inv <- function(x)ifelse(x < 1, (1 - x) ^ -1.5, Inf)
     d.inv <- function(x)ifelse(x < 1, (1 - x) ^ -1.5, Inf)
   } else if (dvg == "Jeffrey") {
@@ -101,10 +101,10 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
 
   } else if (dvg == "LeCam") {
     div <- function(x)0.5 * (1 - x) / (x + 1)
-  }else if (dvg == "user") {
-    d.div0 <- div$d.div0
-    d.inv <- div$d.inv
-
+  } else if (dvg == "user") {
+    div0 <- div.usr$div0
+    inv <- div.usr$inv
+    if (max.l) div <- div.usr$div
   } else stop("The argument 'div' must be one of 'Chi2', 'KL', 'Hellinger', 'Alpha', 'Triangular', 'Jeffrey', 'LeCam' or 'user' ")
 
   z <- f(x_data[, k])
@@ -123,52 +123,70 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
   }
 
   if (min.d) constr <- function(L){
-    RN <- inv.div(pmax(d.div0, L[1] + L[2] * z))
+    RN <- inv(pmax(div0, L[1] + L[2] * z))
     C1 <- sum(p * RN) - 1
     C2 <- sum(p * RN * z) - m
     return(c(C1, C2))
   } else constr <- function(L){
-    RN <- inv.div(pmax(d.div0, L[1] + L[2] * z))
+    RN <- inv(pmax(div0, L[1] + L[2] * z))
     C1 <- sum(p * RN) - 1
     C2 <- sum(p * div(RN)) - theta
     return(c(C1, C2))
   }
 
-  if (is.null(start)) start <- c(L1 = d.div(1), L2 = 0)
+  if (is.null(start) & dvg != "user") start <- c(L1 = d.div(1), L2 = 0)
 
-  # if (!is.null(d.inv)) {
-  #   J <- function(L) {
-  #     d.RN <- d.inv(L[1] + L[2] * z)
-  #     ind <- (L[1] + L[2] * z > d.div0)
-  #     J11 <- sum(p * d.RN * ind)
-  #     J12 <- sum(p * d.RN * z * ind)
-  #     J22 <- sum(p * d.RN * z * z * ind)
-  #     jac <- matrix(c(J11, J12, J12, J22), nrow = 2, byrow = TRUE)
-  #     return(jac)
-  #   }
-  #   sol <- nleqslv::nleqslv(x = start, fn = constr, jac = J, ...)
-  # } else {
-  #   sol <- nleqslv::nleqslv(x = start, fn = constr, ...)
-  #   }
-  sol <- nleqslv::nleqslv(x = start, fn = constr, ...)
+
+  if (exists(d.inv) & min.d) {
+    J <- function(L) {
+      d.RN <- d.inv(L[1] + L[2] * z)
+      ind <- (L[1] + L[2] * z > div0)
+      J11 <- sum(p * d.RN * ind)
+      J12 <- sum(p * d.RN * z * ind)
+      J22 <- sum(p * d.RN * z * z * ind)
+      jac <- matrix(c(J11, J12, J12, J22), nrow = 2)
+      return(jac)
+    } else if (exists(d.inv) & max.l) {
+      J <- function(L) {
+        d.RN <- d.inv(L[1] + L[2] * z)
+        ind <- (L[1] + L[2] * z > div0)
+        J11 <- sum(p * d.RN * ind)
+        J12 <- sum(p * d.RN * z * ind)
+        J22 <- sum(p * d.RN * z * z * ind)
+        J21 <- L[1] * J11 + L[2] * J12
+        J22 <- L[1] * J12 + L[2] * J22
+        jac <- matrix(c(J11, J21, J12, J22), nrow = 2)
+        return(jac)
+    }
+    sol <- nleqslv::nleqslv(x = start, fn = constr, jac = J, ...)
+  } else {
+    sol <- nleqslv::nleqslv(x = start, fn = constr, ...)
+    }
 
   if (sol$termcd != 1) warning(paste("nleqslv terminated with code ", sol$termcd))
 
-  # transform back z?????????????????????
-  w <- p * inv.div(pmax(d.div0, sol$x[1] + sol$x[2] * z)) # p here+++++++++++
+  w <- inv(pmax(div0, sol$x[1] + sol$x[2] * z))
   if(sumRN) w <- w / mean(w)
 
-  m.ac <- sum(w * z)
-  if (normalise == TRUE){
-    m <- min.fz + (max.fz - min.fz) * m
-    m.ac <- min.fz + (max.fz - min.fz) * m.ac
+  if (min.d) {
+    m.ac <- sum(p * w * z)
+    if(normalise == TRUE) {
+      m <- min.fz + (max.fz - min.fz) * m
+      m.ac <- min.fz + (max.fz - min.fz) * m.ac
+    }
+    err <- m - m.ac
+    rel.err <- (err / m) * (m != 0)
+    outcome <- c(required_moment = m, achieved_moment = m.ac, abs_error = err, rel_error = rel.err)
+    constr_moment <- list("k" = k, "m" = m, "f" = f, "div" = dvg)
+  } else {
+    theta.ac <- sum(p * div(w))
+    err <- theta - theta.ac
+    rel.err <- (err / theta) * (theta != 0)
+    outcome <- c(required_divergence = theta, achieved_divergence = theta.ac, abs_error = err, rel_error = rel.err)
+    constr_moment <- list("k" = k, "theta" = theta, "f" = f, "div" = dvg)
   }
-  err <- m - m.ac
-  rel.err <- (err / m) * (m != 0)
-  outcome <- c(required_moment = m, achieved_moment = m.ac, abs_error = err, rel_error = rel.err)
-  print(outcome)
 
-  constr_moment <- list("k" = k, "m" = m, "f" = f, "div" = div)
+  print(outcome)
   constr <- list(constr_moment)
 
   # Name stresses +++++++++++++++++++++++++
@@ -185,7 +203,7 @@ stress_mean_div <- function(x, f = function(x)x, k = 1, m = NULL, theta = NULL, 
   new_weights = list()
   new_weights[[temp]] <- w
 
-  type <- list("moment") #++++++++++++++++++++
+  type <- list("moment")
   my_list <- SWIM:::SWIM("x" = x_data, "new_weights" = new_weights, "type" = type, "specs" = constr)
   if (SWIM:::is.SWIM(x)) my_list <- merge(x, my_list)
 
