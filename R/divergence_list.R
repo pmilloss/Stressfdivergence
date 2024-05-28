717342dd6f81d77f26a4b658072be26fdb5b6b72
Pietro Millossovich <weekendwarrior71@gmail.com>
2024-05-28 14:23:46 +0100

    abc

--- R/divergence_list.R
+++ R/divergence_list.R
@@ -1,7 +1,36 @@
 #' divergence
 #'
+#' @param dvg Character. One of "\code{Chi2}", "\code{KL}", "\code{revKL}", "\code{Hellinger}", "\code{Alpha}" or "\code{user}". See 'Details'
+#'
+#' @param alpha the value of the alpha parameter for the "Alpha" divergence
+#'
+#' @author Pietro Millossovich
+#'
+#' @details The following divergence functions are considered:
+#'
+#' Chi2 (\code{dvg=Chi2}):
+#'
+#' \deqn{h(x)=x^2-1}
+#'
+#' Kullback-Leibler (\code{dvg=KL}):
+#'
+#' \deqn{h(x)=x \dot \log(x)}
+#'
+#' reverse Kullback-Leibler (\code{dvg=revKL}):
+#'
+#' \deqn{h(x)=-\log(x)}
+#'
+#' Hellinger (\code{dvg=Hellinger}):
+#'
+#' \deqn{h(x)=(1 - x) ^ {-2}}.
+#'
+#' Alpha (\code{dvg=Alpha}):
+#'
+#' \deqn{h(x)=\frac{x ^ \alpha - \alpha * (x - 1) - 1}{\alpha * (\alpha - 1)}}
 #'
 #'
+#' @examples
+#' # example code
 #'
 #'
 #'
@@ -14,54 +43,44 @@ divergence <- function(dvg = c("Chi2", "KL", "revKL", "Hellinger", "Alpha"), alp
     div <- function(x)ifelse(x >= 0, x ^ 2 - 1, Inf)
     d.div <- function(x)2 * x
     inv <- function(x)0.5 * x
-    div0 <- d.div(0)
     d.inv <- function(x)0.5
   } else if (dvg == "KL" | (dvg == "Alpha" & identical(alpha, 1))) {
     div <- function(x)ifelse(x > 0, x * log(x), Inf)
     inv <- function(x)exp(x - 1)
     d.div <- function(x)ifelse(x > 0, 1 + log(x), -Inf)
-    div0 <- -Inf
     d.inv <- function(x)inv(x)
   } else if (dvg == "revKL" | (dvg == "Alpha" & identical(alpha, 0))) {
     div <- function(x)ifelse(x > 0, -log(x), Inf)
     inv <- function(x)ifelse(x < 0, -1 / x, Inf)
     d.div <- function(x)ifelse(x > 0, -1 / x, -Inf)
-    div0 <- -Inf
     d.inv <- function(x)ifelse(x < 0, 1 / x ^ 2, Inf)
   } else if (dvg == "Hellinger") {
     div <- function(x)ifelse(x >= 0, (sqrt(x) - 1) ^ 2, Inf)
     inv <- function(x)ifelse(x < 1, 1 / (1 - x) ^ 2, Inf)
     d.div <- function(x)ifelse(x > 0, 1 - 1 / sqrt(x), -Inf)
-    div0 <- -Inf
     d.inv <- function(x)ifelse(x < 1, 2 / (1 - x) ^ 3, Inf)
   } else if (dvg == "Alpha"){
+    h <- function(x)(x ^ alpha - alpha * (x - 1) - 1) / (alpha * (alpha - 1))
+    h1 <- function(x)(x ^ (alpha - 1) - 1) / (alpha -1)
+    g <- function(x)(1 + x * (alpha - 1)) ^ (1 / (alpha - 1))
+    g1 <- function(x)(1 + x * (alpha - 1)) ^ ((2 - alpha) / (alpha - 1))
+    d0 <- -1 / (alpha - 1)
+    dvg <- paste(dvg, " a=", alpha)
     if (alpha > 1){
-      div <- function(x)ifelse(x >= 0, (x ^ alpha - alpha * (x - 1) - 1) / (alpha * (alpha - 1)), Inf)
-      div0 <- -1 / (alpha - 1)
-      inv <- function(x)ifelse(x > div0, (1 + x * (alpha - 1)) ^ (1 / (alpha - 1)), 0)
-      d.div <- function(x)ifelse(x > 0, (x ^ (alpha - 1) - 1) / (alpha -1), div0)
-      d.inv <- function(x)ifelse(x > div0, (1 + x * (alpha - 1)) ^ (1 / (alpha - 1) - 1), )
-      dvg <- paste(dvg, " a=", alpha)
-    } else if (alpha > 1) {
-      div <- function(x)ifelse(x >= 0, (x ^ alpha - alpha * (x - 1) - 1) / (alpha * (alpha - 1)), Inf)
-      div0 <- -1 / (alpha - 1)
-      inv <- function(x)ifelse(x > div0, (1 + x * (alpha - 1)) ^ (1 / (alpha - 1)), 0)
-      d.div <- function(x)ifelse(x > 0, (x ^ (alpha - 1) - 1) / (alpha -1), div0)
-      d.inv <- function(x)ifelse(x > 0, (1 + x * (alpha - 1)) ^ (1 / (alpha - 1) - 1), )
-      dvg <- paste(dvg, " a=", alpha)
-    } else if (dvg == "Alpha" & alpha > 2) {
-      div <- function(x)(x ^ alpha - alpha * (x - 1) - 1) / (alpha * (alpha - 1))
-      inv <- function(x)(-2 * x * (1 + alpha)) ^ (-2 / (1 + alpha))
-      d.div <- function(x)(-0.5 * (x ^ (-0.5 * (1 + alpha))) / (1 + alpha))
-      div0 <- d.div(0)
-      d.inv <- function(x)4 * (-2 * x * (1 + alpha)) ^ (- (3 + alpha) / (1 + alpha))
-      dvg <- paste(dvg, " a=", alpha)
+      div <- function(x)ifelse(x >= 0, h(x), Inf)
+      inv <- function(x)ifelse(x > d0, g(x), 0)
+      d.div <- function(x)ifelse(x >= 0, h1(x), d0)
+      if (alpha > 2) d.inv <- function(x)ifelse(x > d0, g1(x), Inf) else d.inv <- function(x)ifelse(x > d0, g1(x), 0)
+    } else if (alpha < 1) {
+      div <- function(x)ifelse(x > 0, h(x), Inf)
+      inv <- function(x)ifelse(x < d0, g(x), Inf)
+      d.div <- function(x)ifelse(x > 0, h1(x), -Inf)
+      d.inv <- function(x)ifelse(x < d0, g1(x), Inf)
     }
+    dvg <- paste(dvg, " alpha=", alpha)
+  }
 
-
-}
-
-  diver = list(div = div, inv = inv, div0 = div0, d.div = d.div, d.inv = d.inv)
+  diver = list(div = div, inv = inv, d.div = d.div, d.inv = d.inv)
   return(diver)
 
   }
